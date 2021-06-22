d9724a3f529bdd1c0a80e21f42b993c14854c36d
Benjamin Vedder <benjamin@vedder.se>
2021-06-22 15:16:13 +0200

    Changed voltage divider value

--- hwconf/hw_18s_light.h
+++ hwconf/hw_18s_light.h
@@ -55,7 +55,7 @@
 #define HW_SHUNT_RES			(1.0e-3)
 #define HW_SHUNT_AMP_GAIN		(50.0)
 #define V_REG					3.3
-#define R_CHARGE_TOP			(520e3 + 2.5e3 + 100.0)
+#define R_CHARGE_TOP			(316e3 + 2.5e3 + 100.0)
 #define R_CHARGE_BOTTOM			(10e3)
 
 // LEDs
